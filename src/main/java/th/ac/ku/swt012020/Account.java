571169c3e881b74eaa083c4ecf6f4d4d46b73a14
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2020-11-28 10:57:20 +0700

    Implement account suspension

--- src/main/java/th/ac/ku/swt012020/Account.java
+++ src/main/java/th/ac/ku/swt012020/Account.java
@@ -1,15 +1,33 @@
 package th.ac.ku.swt012020;
 
 public class Account {
-    private Person person;
+    private Boolean suspended;
+    private Person owner;
 
-    public Account(Person person) {
-        this.person = person;
+    public Account(Person owner) {
+        this.owner = owner;
+        suspended = false;
     }
 
     public void suspend() {
+        if (!isSuspended()) {
+            suspended = true;
+        }
+        else {
+            throw new RuntimeException("Account is already suspended");
+        }
     }
 
     public void unsuspend() {
+        if (isSuspended()) {
+            suspended = false;
+        }
+        else {
+            throw new RuntimeException("Account is not suspended");
+        }
+    }
+
+    public boolean isSuspended() {
+        return suspended;
     }
 }
